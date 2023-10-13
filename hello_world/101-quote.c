5b4711dab3f8e2e7d10cee4bc897a0391a8a663d
Ferlandoo <contact@ferlando.me>
2023-10-13 03:11:11 -0700

    sec vers

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -6,7 +7,7 @@
  */
 int main(void)
 {
-        char str[58] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-        puts(str);
-        return (1);
+	static const char hello[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fwrite(hello, sizeof(hello) - 1, 1, stdout);
+	return (0);
 }
