276a7f39452d02a65625ca920a9659d2f283ecbb
Ferlandoo <contact@ferlando.me>
2023-10-13 03:32:55 -0700

    fixxxxx

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 /**
@@ -9,6 +9,7 @@
 int main(void)
 {
 	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(1, str, strlen(str));
 	return (1);
 }
