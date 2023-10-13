91a82db4b79ab0172f58bb6c2c8ecef7bea11854
Ferlandoo <contact@ferlando.me>
2023-10-13 01:38:25 -0700

    unix1

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 1 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
 	return (0);
 }
