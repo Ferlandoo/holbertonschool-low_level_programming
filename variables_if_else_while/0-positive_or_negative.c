5c28092b2b6a94875dc270f516880f5ae50f15a9
Ferlandoo <contact@ferlando.me>
2023-10-16 09:48:04 -0700

    fix

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -8,15 +8,15 @@
  */
 int main(void)
 {
-	int n=0;
+	int n = 0;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	if (n>0)
+	if (n > 0)
 	{
 		printf("%u is positive\n", n);
 	}
-	else if (n<0)
+	else if (n < 0)
 	{
 		printf("%u is negative\n", n);
 	}
