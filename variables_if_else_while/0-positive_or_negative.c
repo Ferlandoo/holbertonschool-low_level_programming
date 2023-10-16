58807eec762bd2a4f043e13ed202c246d879f801
Ferlandoo <contact@ferlando.me>
2023-10-16 09:54:16 -0700

    d

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%u is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("%lu is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	else
 	{
-		printf("%u is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	return (0);
 }
