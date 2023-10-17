b90a1881e2047d297404cdd63395fb9ec0579c1d
Ferlandoo <contact@ferlando.me>
2023-10-17 04:04:44 -0700

    10

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = a + 1; b <= 9; b++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i < 8 || j < 9)
+				putchar(',');
+			putchar(' ');
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
