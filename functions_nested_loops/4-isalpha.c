d1100c86efe524d15a59bdc71e68adca4e84efc3
Ferlandoo <contact@ferlando.me>
2023-10-18 03:05:44 -0700

    4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - checks for an alphabetic character; in the standard "C" locale
+ * @c: the character to check
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
