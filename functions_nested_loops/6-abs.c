82ff040ad44219f46b44fce3c7cf4e4c39792399
Ferlandoo <contact@ferlando.me>
2023-10-18 03:57:26 -0700

    6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - function computes the absolute value of the integer argument j
+ * Return: 1 n is greater than zero, 0 if n is zero, -1 if n is less then zero
+ */
+int _abs(int n)
+{
+	if (n >= 0 ){
+	return (n);
+	}
+	return (-n);
+}
