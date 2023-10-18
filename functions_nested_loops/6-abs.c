4a310bef2d4ed2c67ccfc0797401ec6e5c4c207e
Ferlandoo <contact@ferlando.me>
2023-10-18 04:03:27 -0700

    betty

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,11 +2,13 @@
 
 /**
  * _abs - function computes the absolute value of the integer argument j
- * Return: 1 n is greater than zero, 0 if n is zero, -1 if n is less then zero
+ * @n: the character to check
+ * Return: 1 n is greater or equal zero,  -1 if n is less then zero
  */
 int _abs(int n)
 {
-	if (n >= 0 ){
+	if (n >= 0)
+	{
 	return (n);
 	}
 	return (-n);
