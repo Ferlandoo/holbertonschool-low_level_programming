13167d9f442ed09872798e906cc1d98c4ab2a2f1
Ferlandoo <contact@ferlando.me>
2023-10-18 05:11:34 -0700

    clock

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the int to extract the last digit from
+ * Return: value of the last digit
+ */
+void jack_bauer(void)
+{
+	int a, b;
+
+	for (a = 0; a < 24; a++)
+	{
+		for (b = 0; b < 60; b++)
+		{
+			if (a < b)
+			{
+				_putchar((a / 10) + 48);
+				_putchar((a % 10) + 48);
+				_putchar(':');
+				_putchar ((b / 10) + 48);
+				_putchar ((b % 10) + 48);
+				_putchar('\n');
+			}
+		}
+	}
+}
