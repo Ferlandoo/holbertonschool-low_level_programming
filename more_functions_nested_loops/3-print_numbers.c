36481e8aa572141d39d83b885b8dd6e81f8ffccf
Ferlandoo <contact@ferlando.me>
2023-10-19 05:35:29 -0700

    print number

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ * @a: get character a
+ * @b: get character b
+ * Return: result of integers
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		_putchar(n + 48);
+	_putchar('\n');
+}
