e2cbbfa63116459866cf6a8d43da2ce90f5e6048
Ferlandoo <contact@ferlando.me>
2023-10-19 05:11:48 -0700

    betty

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Entry point
- * Return:0
+ * _isupper - check for uppercase characters
+ * @c: check character
+ * Return:1 for uppercase and 0 for lowercase
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <='Z')
-		return 1;
-	return 0;
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
 }
