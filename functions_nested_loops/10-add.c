7df29d9fceba83416ecd388caa39412ec234c8b6
Ferlandoo <contact@ferlando.me>
2023-10-19 01:13:52 -0700

    fix

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -14,6 +14,6 @@ int print_last_digit(int n)
 	a = n % 10;
 	if (a < 0)
 		a = -a;
-	_putchar(a + '0');
+	_putchar(a + 48);
 	return (a);
 }
