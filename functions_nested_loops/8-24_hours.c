3c719d96938dee9a18c74d1a2a54fdd2af06ec74
Ferlandoo <contact@ferlando.me>
2023-10-19 00:27:46 -0700

    betty

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -12,12 +12,12 @@ void jack_bauer(void)
 	{
 		for (b = 0; b < 60; b++)
 		{
-				_putchar((a / 10) + 48);
-				_putchar((a % 10) + 48);
-				_putchar(':');
-				_putchar ((b / 10) + 48);
-				_putchar ((b % 10) + 48);
-				_putchar('\n');
+			_putchar((a / 10) + 48);
+			_putchar((a % 10) + 48);
+			_putchar(':');
+			_putchar ((b / 10) + 48);
+			_putchar ((b % 10) + 48);
+			_putchar('\n');
 		}
 	}
 }
