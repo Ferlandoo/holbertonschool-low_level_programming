c8c17c5da65f33f5f6bce6f3fad293dbd931e256
Ferlandoo <contact@ferlando.me>
2023-10-23 13:21:04 -0700

    puts2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,21 @@
 #include "main.h"
 /**
- * rev_string - print reverse string
- * @s: reverse string
+ * puts2 - print numbers and new line
+ * @str: input
  */
 void puts2(char *str)
 {
+	int len = 0;
+	int i, last_index;
 
+	while (str[len] != '\0')
+		len++;
+	last_index = len - 1;
+
+	for (i = 0; i <= last_index; i++)
+	{
+		if (str[i] % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
 }
