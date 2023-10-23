16f184900607d2a61bb471e6fe3a4722dfd66374
Ferlandoo <contact@ferlando.me>
2023-10-23 04:32:05 -0700

    rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - print reverse text
+ * @s: reverse text
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+	int i, last_index;
+
+	i = 0;
+	while (s[len] != '\0')
+		len++;
+	last_index = len - 1;
+
+	for (i = last_index; i >= 0; i--)
+		putchar(s[i]);
+	putchar('\n');
+}
