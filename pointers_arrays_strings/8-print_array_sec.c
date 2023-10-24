677f4eb4eb34bff1f66d12ece72afb7998cf9f72
Ferlandoo <contact@ferlando.me>
2023-10-24 02:21:11 -0700

    sec vers

--- pointers_arrays_strings/8-print_array_sec.c
+++ pointers_arrays_strings/8-print_array_sec.c
@@ -11,8 +11,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
+		printf("%d", a[i]);
 		if (i != (n - 1))
-			printf("%d, ", a[i]);
+		{
+			printf(", ");
+		}
 	}
-	printf("%d \n", a[n-1]);
+	_putchar('\n');
 }
