5f80b43da9e9e1fd2df9cce1ee54bf8fb4e9c53c
Ferlandoo <contact@ferlando.me>
2023-10-24 01:10:24 -0700

    puts half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts_half - print second half of string
+ * @str: half of string
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int half;
+
+	while (str[len] != '\0')
+		len++;
+	half = len / 2;
+	while (half < len)
+	{
+		_putchar(str[half]);
+		half++;
+	}
+	_putchar('\n');
+}
