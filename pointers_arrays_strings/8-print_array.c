6caec5d3cb2147f11b43834bcfd770634bf11355
Ferlandoo <contact@ferlando.me>
2023-10-24 02:33:40 -0700

    fix

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,12 +11,10 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i < (n - 1))
-		{
-			printf("%d, ", a[i]);
-		}
-		else
+		if (i == n - 1)
 			printf("%d", a[i]);
+		else
+			printf("%d, ", a[i]);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
