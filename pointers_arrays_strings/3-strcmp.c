031b80c558fba5a7348d2537f5cc324dedd2f364
Ferlandoo <contact@ferlando.me>
2023-10-25 03:55:15 -0700

    fix betty

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,14 +11,14 @@ int _strcmp(char *s1, char *s2)
 	int i = 0;
 	int diff = 0;
 
-	while(s1[i] != '\0' || s2[i] != '\0')
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
-		if(s1[i] != s2[i])
+		if (s1[i] != s2[i])
 		{
-			diff= s1[i] - s2[i];
+			diff = s1[i] - s2[i];
 			break;
 		}
 		i++;
 	}
-	return(diff);
+	return (diff);
 }
