cc6121faafce145f3d7aedc9b08c23211dfd2b05
Ferlandoo <contact@ferlando.me>
2023-10-31 02:45:57 -0700

    fix 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,21 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * is_prime - add an exception for i start from 2
- * @n: input
- * Return: n number and i=2 for other function
- */
-int is_prime(int n)
-{
-	return (is_prime(n, 2));
-}
-/**
- * is_prime_number - check if number is prime
+ * is_prime - check if number is prime
  * @n: input
  * @i: number for exeption that number is prime
  * Return: 1 if number is prime, otherwise return 0
  */
-int is_prime_number(int n, int i)
+int is_prime(int n, int i)
 {
 	if (n <= 1)
 	{
@@ -31,4 +22,13 @@ int is_prime_number(int n, int i)
 	}
 	else
 		return (is_prime(n, i + 1));
+}
+/**
+ * is_prime_number - add an exception for i start from 2
+ * @n: input
+ * Return: n number and i=2 for other function
+ */
+int is_prime_number(int n)
+{
+        return (is_prime(n, 2));
 }
