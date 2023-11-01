84a83e592bcf595c17ea239fda9c49b7ab9ca9ba
Ferlandoo <contact@ferlando.me>
2023-11-01 04:49:32 -0700

    fix

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: The command line arguments
+ * Return: 0 (Success)
+ */
+
 int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s", argv[i]);
+		if (i == 0)
+		{
+			printf("%s", argv[i]);
+		}
+
 	}
 	printf("\n");
 	return (0);
