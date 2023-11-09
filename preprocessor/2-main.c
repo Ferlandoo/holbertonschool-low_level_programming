8fa5b6cfaec160971239ff6932906209631aac63
Ferlandoo <contact@ferlando.me>
2023-11-09 01:45:18 -0800

    file name

---
+++ preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the file that was compiled with new line
+ * Return: name of file with new line
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
