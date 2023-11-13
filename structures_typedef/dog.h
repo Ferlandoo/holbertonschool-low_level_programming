3ba947f53c34b3496cac652e51448367ef322e98
Ferlandoo <contact@ferlando.me>
2023-11-13 01:53:01 -0800

    fix doc

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - a structure representing a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ * Description: This structure represents a dog and contains information about
+ * its name, age, and owner.
+ */
 struct dog
 {
 	char *name;
