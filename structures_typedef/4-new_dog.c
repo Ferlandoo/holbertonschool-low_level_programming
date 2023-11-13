c1012e30668a1ecc94c767d69276efd293c18aaa
Ferlandoo <contact@ferlando.me>
2023-11-13 05:45:53 -0800

    free

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,6 +3,14 @@
 #include <string.h>
 #include "dog.h"
 
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: pointer to the new dog_t
+ */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *sec_dog = malloc(sizeof(dog_t));
@@ -11,6 +19,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (sec_dog == NULL || sec_name == NULL || sec_owner == NULL)
 	{
+		free(sec_dog);
+		free(sec_name);
+		free(sec_owner);
 		return (NULL);
 	}
 	strcpy(sec_name, name);
