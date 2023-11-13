9372783642c7f42436b60f09b45bc2de50ffeeed
Ferlandoo <contact@ferlando.me>
2023-11-13 02:57:45 -0800

    ex3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,12 +8,12 @@
  * Description: This structure represents a dog and contains information about
  * its name, age, and owner.
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
