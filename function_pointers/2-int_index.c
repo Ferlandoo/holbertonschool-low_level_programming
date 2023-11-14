182cf0a273a931fc18d69c775635e468dee1bd58
Ferlandoo <contact@ferlando.me>
2023-11-14 12:29:14 -0800

    null

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -15,15 +15,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array)
+	if (cmp == NULL)
+		return (-1);
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
 	{
-		if (size <= 0)
-			return (-1);
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i]) != 0)
-				return (i);
-		}
+		if (cmp(array[i]) != 0)
+			return (i);
 	}
 	return (-1);
 }
