53c4bebde5e67dd83043c1e6e88a45d2787a4ce5
Ferlandoo <contact@ferlando.me>
2023-11-15 02:49:10 -0800

    fix betty

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,14 +1,18 @@
 #ifndef CALC_H
 #define CALC_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 int op_add(int a, int b);
 int op_sub(int a, int b);
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,24 +1,59 @@
 #include "3-calc.h"
 
+/**
+ * op_add - returns the sum of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of a and b
+ */
+
 int op_add(int a, int b)
 {
     return (a + b);
 }
+
+/**
+ * op_sub - returns the difference of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: difference of a and b
+ */
 
 int op_sub(int a, int b)
 {
     return (a - b);
 }
 
+/**
+ * op_mul - returns the product of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: product of a and b
+ */
+
 int op_mul(int a, int b)
 {
     return (a * b);
 }
 
+/**
+ * op_div - returns the result of the division of a by b
+ * @a: first integer
+ * @b: second integer
+ * Return: result of the division of a by b, or error if b is zero
+ */
+
 int op_div(int a, int b)
 {
     return (a / b);
 }
+
+/**
+ * op_mod - returns the remainder of the division of a by b
+ * @a: first integer
+ * @b: second integer
+ * Return: remainder of the division of a by b, or error if b is zero
+ */
 
 int op_mod(int a, int b)
 {
