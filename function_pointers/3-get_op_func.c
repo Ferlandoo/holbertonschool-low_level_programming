373df3d612fd84ba93803df2d035ec87012ef672
Ferlandoo <contact@ferlando.me>
2023-11-15 02:42:56 -0800

    fix betty

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,5 +1,11 @@
 #include "3-calc.h"
 
+/**
+ * get_op_func - selects the correct function to perform the operation
+ * @s: operator passed as argument to the program
+ * Return: pointer to the function that corresponds to the operator given as a parameter
+ */
+
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
