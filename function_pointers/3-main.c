f0f6688ed6fdeb8f1e00f003a417a7fcc39fbc0b
Ferlandoo <contact@ferlando.me>
2023-11-15 02:30:52 -0800

    calc

---
+++ function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+
+/**
+ * main - Entry point. Performs basic arithmetic operations on two numbers.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: Always 0 (Success).
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	int (*op)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[2]);
+	num2 = atoi(argv[4]);
+	op = get_op_func(argv[3]);
+	if (op == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[3][0] == '/' || argv[3][0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", op(num1, num2));
+	return (0);
+}
