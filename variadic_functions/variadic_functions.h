7d33e23c9e91747bc7283e97e11e73439365e469
Ferlandoo <contact@ferlando.me>
2023-11-16 01:07:04 -0800

    fix lib

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,6 +2,7 @@
 #define VARIADIC
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
