8e494b08859de9e90168600d37ecce0a06bc7f92
Ferlandoo <contact@ferlando.me>
2023-11-16 02:31:14 -0800

    fix break

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -30,6 +30,7 @@ void print_all(const char * const format, ...)
 				if(str == NULL)
                                 {
                                         printf("(nil)");
+					break;
                                 }
 				if(str)
 				{
