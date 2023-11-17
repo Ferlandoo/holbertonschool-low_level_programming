0b57a7b121aca57b2f8ce88eef657fcce8f39ae4
Ferlandoo <contact@ferlando.me>
2023-11-17 01:49:36 -0800

    fix

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -12,34 +12,31 @@ void print_all(const char * const format, ...)
 	va_list list;
 
 	va_start(list, format);
-	if (format)
+	while (format[i])
 	{
-		while (format[i])
+		switch (format[i])
 		{
-			switch (format[i])
-			{
-				case 'c':
-					printf("%s%c", sep, va_arg(list, int));
-					break;
-				case 'i':
-					printf("%s%d", sep, va_arg(list, int));
-					break;
-				case 'f':
-					printf("%s%f", sep, va_arg(list, double));
-					break;
-				case 's':
-					str = va_arg(list, char *);
-					if (!str)
-						str = "(nil)";
-					printf("%s%s", sep, str);
-					break;
-				default:
-					i++;
-					continue;
-			}
-			sep = ", ";
-			i++;
+			case 'c':
+				printf("%s%c", sep, va_arg(list, int));
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(list, int));
+				break;
+			case 'f':
+				printf("%s%f", sep, va_arg(list, double));
+				break;
+			case 's':
+				str = va_arg(list, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s%s", sep, str);
+				break;
+			default:
+				i++;
+				continue;
 		}
+		sep = ", ";
+		i++;
 	}
 	printf("\n");
 	va_end(list);
