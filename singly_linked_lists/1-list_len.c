802b9903d79d3de6109fe0f3ff1f706065a61a60
Ferlandoo <contact@ferlando.me>
2023-11-27 14:46:18 -0800

    fix betty

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * print_len - returns the number of elements in a linked list
+ * list_len - returns the number of elements in a linked list
  * @h: pointer to the list_t list
  * Return: the number of elements in list_t list
  */
