67ffa59c865b9e7e70f1a9033d5089c9ea925246
Ferlandoo <contact@ferlando.me>
2023-11-27 14:43:39 -0800

    add exercise 1. List length

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
+ * Return: the number of elements in list_t list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
