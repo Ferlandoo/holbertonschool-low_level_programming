8248a4fd8b0e02f3d9ecce99f131106663666393
Ferlandoo <contact@ferlando.me>
2023-11-30 03:14:10 -0800

    add exercise 6. Sum list

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of all the data (n) of a linked list
+ * @head: linked list of type dlistint_t to sum
+ * Return: sum of all the data (n) of a linked list
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	size_t sum = 0, num = 0;
+
+	while (head != NULL)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
+}
