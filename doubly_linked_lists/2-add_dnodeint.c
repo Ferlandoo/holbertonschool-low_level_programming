5da8acbe3e36429f7ae16cf1dec81d92f290a202
Ferlandoo <contact@ferlando.me>
2023-11-29 16:18:46 -0800

    add if cond

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -16,6 +16,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new->n = n;
 	new->next = *head;
+	new->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = new;
 	*head = new;
 	return (new);
 }
