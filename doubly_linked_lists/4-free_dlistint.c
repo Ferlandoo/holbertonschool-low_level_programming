d6fb266dd09d92ab1e122ed5a41eba8623b8895c
Ferlandoo <contact@ferlando.me>
2023-11-29 16:36:48 -0800

    fix description

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * free_listint - frees a linked list
- * @head: listint_t list to be freed
+ * free_dlistint - frees a linked list
+ * @head: dlistint_t list to be freed
  */
 void free_dlistint(dlistint_t *head)
 {
