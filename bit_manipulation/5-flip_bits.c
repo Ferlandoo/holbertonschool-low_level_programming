92a6cd62b3faea1eb314feee875bc1652a7feca5
Ferlandoo <contact@ferlando.me>
2023-12-02 15:38:41 -0800

    fix betty style

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -8,16 +8,17 @@
  * @m: Second number.
  * Return: Number of bits to flip.
  */
+
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned int count = 0;
-    unsigned long int xor = n ^ m;
+	unsigned int count = 0;
+	unsigned long int xor = n ^ m;
 
-    while (xor > 0)
-    {
-        if (xor & 1)
-            count++;
-        xor >>= 1;
-    }
-    return (count);
+	while (xor > 0)
+	{
+		if (xor & 1)
+			count++;
+		xor >>= 1;
+	}
+	return (count);
 }
