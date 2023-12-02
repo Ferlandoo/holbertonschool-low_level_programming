4265f3dd0c38f135b9770f70a4748239a73d53c1
Ferlandoo <contact@ferlando.me>
2023-12-02 08:21:15 -0800

    remove %

--- bit_manipulation/1-print_binary-2.c
+++ bit_manipulation/1-print_binary-2.c
@@ -9,11 +9,7 @@
 
 void print_binary(unsigned long int n)
 {
-	if ((n >> 1) == 0)
-	{
-		_putchar('0' + (n & 1));
-	}
-	else
+	if (n > 1)
 		print_binary(n >> 1);
-	_putchar('0' + (n & 1));
+	printf("%lu", n & 1);
 }
