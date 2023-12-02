ff3dc61ad031b3cb6eaea2634685a609065d13da
Ferlandoo <contact@ferlando.me>
2023-12-02 08:00:19 -0800

    add ex.8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -9,27 +9,30 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *temp = *head;
-	unsigned int i = 0;
+	dlistint_t *temp;
+	unsigned int i;
 
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
+	temp = *head;
 	if (index == 0)
 	{
 		*head = temp->next;
-		if (temp->next != NULL)
-			temp->next->prev = NULL;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(temp);
 		return (1);
 	}
-	while (i < index)
+	for (i = 0; i < index; i++)
 	{
 		if (temp == NULL)
 			return (-1);
 		temp = temp->next;
-		i++;
 	}
-	temp->prev->next = temp->next;
 	if (temp->next != NULL)
 		temp->next->prev = temp->prev;
+	if (temp->prev != NULL)
+		temp->prev->next = temp->next;
+	free(temp);
 	return (1);
 }
