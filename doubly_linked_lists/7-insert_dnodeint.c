a3297efa9939c1a31fb78879d2698fdf97100bd9
Ferlandoo <contact@ferlando.me>
2023-12-02 07:57:36 -0800

    add some lines

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,32 +12,37 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current_node = *h;
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-	unsigned int i = 0;
+	dlistint_t *new_node, *temp;
+	unsigned int i;
 
+	if (h == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	if (idx == 0)
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	if (idx == 0 || *h == NULL)
 	{
-		new_node->prev = NULL;
-		new_node->next = current_node;
+		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
 		*h = new_node;
 		return (new_node);
 	}
-	while (current_node != NULL)
+	temp = *h;
+	for (i = 0; i < idx - 1 && temp != NULL; i++)
+		temp = temp->next;
+	if (temp == NULL)
 	{
-		if (i == idx - 1)
-		{
-			new_node->prev = current_node;
-			new_node->next = current_node->next;
-			current_node->next = new_node;
-			return (new_node);
-		}
-		current_node = current_node->next;
-		i++;
+		free(new_node);
+		return (NULL);
 	}
-	free(new_node);
-	return (NULL);
+	new_node->next = temp->next;
+	new_node->prev = temp;
+	if (temp->next != NULL)
+		temp->next->prev = new_node;
+	temp->next = new_node;
+	return (new_node);
 }
