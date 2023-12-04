464aa20b8c225e6de1f00d0e5c3623ab2cc1f93a
Ferlandoo <contact@ferlando.me>
2023-12-04 02:38:45 -0800

    fix betty

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,21 +9,21 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    int fd, w, len = 0;
+	int fd, w, len = 0;
 
-    if (filename == NULL)
-        return (-1);
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fd == -1)
-        return (-1);
-    if (text_content != NULL)
-    {
-        while (text_content[len])
-            len++;
-        w = write(fd, text_content, len);
-        if (w == -1)
-            return (-1);
-    }
-    close(fd);
-    return (1);
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+		w = write(fd, text_content, len);
+		if (w == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
 }
