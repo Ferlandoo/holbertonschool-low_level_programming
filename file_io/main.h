11cae7f55a4c02033a291b49508a62acde0611c8
Ferlandoo <contact@ferlando.me>
2023-12-05 05:00:55 -0800

    add prototype

--- file_io/main.h
+++ file_io/main.h
@@ -4,9 +4,11 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void cp(char *file_from, char *file_to);
 
 #endif
