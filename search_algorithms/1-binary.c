7f040c52ed8f690e02b259a60f9f3bebfee351ae
Ferlando <contact@ferlando.me>
2024-01-14 21:37:36 +0100

    add null

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -14,6 +14,8 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t low = 0, high = size - 1, middle;
 
+	if (array == NULL)
+		return (-1);
 	while (low <= high)
 	{
 		printf("Searching in array: ");
